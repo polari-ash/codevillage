8456b643a544ce3a2be2b8c169c4a756b8b19940
yukino-sasaki <yukisnowman.18@gmail.com>
2021-01-23 13:16:46 +0900

    ハンバーガーメニューを追加してみました。見本がなかったので自分なりにオリジナルにしてみました。scssでデザインが終わった後はそれほど気にならなかったのですが、jQueryを使った瞬間コードの量がとても多くなってしまいました。scss,jQyeryともにもう少し減らせたと思いますが、ひとまず提出することにしました。

---
+++ hair/hair.js
@@ -0,0 +1,30 @@
+// $(function () {
+// $('.ham').on('click', function () {
+// $('nav .li').toggleClass('is-active');
+// })
+// });
+
+$(function () {
+    $('.ham').on("click", function () {
+        $('.nav').toggleClass('hamburger');
+        $('.nav-list').toggleClass('open');
+        $('.menu-list').toggleClass('open-menu');
+        $('.bar-top').toggleClass('open-list');
+        $('.bar-mid').toggleClass('open-mid');
+        $('.bar-bottom').toggleClass('open-bottom');
+        $('.bar').toggleClass('open-bottom');
+    });
+});
+
+$(function () {
+    // #で始まるリンクをクリックしたら実行されます
+    $('a[href^="#"]').click(function () {
+        // スクロールの速度
+        var speed = 400; // ミリ秒で記述
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({ scrollTop: position }, speed, 'swing');
+        return false;
+    });
+});
